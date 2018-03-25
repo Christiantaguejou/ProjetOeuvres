1a53294cc1e717859b3606203fd3c67b8920c154
Mark Kpamy <markkpamy@gmail.com>
2018-03-25 19:17:52 +0200

    clean

--- ProjetOeuvreSpringJPA/src/main/java/test.java
+++
@@ -1,9 +0,0 @@
-import com.epul.oeuvres.dao.Service;
-
-public class test {
-
-    public static void main(String[] args){
-        Service service =new Service();
-        service.test();
-    }
-}
